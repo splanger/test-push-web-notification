97ba29879991bfb8de52c19d505f100efb2b9e99
Yuri Kroz <splanger@gmail.com>
2020-10-17 15:37:32 -0700

    Add sample subscriber details

--- push.js
+++ push.js
@@ -9,6 +9,13 @@ let vapidKeys = {
 push.setVapidDetails('mailto:yuri.kroz@outlook.com', vapidKeys.publicKey ,vapidKeys.privateKey)
 
 // List of subscribers. It is the returned value from calling "sw.pushManager.subscribe(...)" on the client side. 
-let sub = {};
+let sub = {
+    endpoint: 'https://fcm.googleapis.com/fcm/send/fLU-Z4IGrnU:APA91bEFyNtoMdKRDPF2sJa1FBIVJgv6AS_u3GgiFwgUmw2aEDHtSY8VyQdEtoq6NiC4EwWAFLrd13sE87TdI3Ga0VRMdszYnjGurkxzlbCdIOa00Y4txqH_g6TxifpBmJ7nQtghm0mf',
+    expirationTime: null,
+    keys: {
+        p256dh: 'BGzOQ88tMXbzpS_bZPIeIeUwwXRAHBNGjRrpriQk6YyRVPpOZjEGBNqfcFU3l8cY-_HGuUwU1pPpsAkwS09T_vI',
+        auth: 'Z7nPqr5sAqxf3Eme_Mo6PQ'
+    }
+};
 
 push.sendNotification(sub, 'test-message')
